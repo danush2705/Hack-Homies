34f35ed625b348190be942201af9581d2ca4f36c
Danush SV <160326467+danush2705@users.noreply.github.com>
2024-02-23 14:04:50 +0530

    Delete Predicitve Model/dash_board.py

--- Predicitve Model/dash_board.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from PIL import Image
-
-# Assuming you have an image named 'your_image.png' located in the current directory
-image = Image.open("C:\\Users\\svdan\\OneDrive\\Desktop\\Bolt 2.0\\dash.png")
-
-caption = 'DashBoard'
-
-st.image(image, caption=caption)
